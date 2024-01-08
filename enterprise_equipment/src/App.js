7136eba52d02d10645006a6fef26541f6cabf0cf
Roflianochka <maximbelaev831@gmail.com>
2024-01-08 13:25:27 +0300

    feat:  add App.js file

---
+++ enterprise_equipment/src/App.js
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
